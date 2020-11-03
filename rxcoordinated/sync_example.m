dc5c1a9ba83ea132db74509479960c4a66d42c91
Swaroop Appadwedula <swaroop@ll.mit.edu>
2020-11-03 10:35:14 -0500

    fixed yhatnet,yhatcnet, edited plots

--- rxcoordinated/sync_example.m
+++ rxcoordinated/sync_example.m
@@ -37,10 +37,10 @@ datadir = '/afs/mitll/data/2239-2601/data';
 rx_dir = 'fromStrokkur/Experiments/30Sept2020/iqdata/C';
 
 %J/S 41dB
-rx_filename = 'rx_file_30Sept2020_expC_noisegenon43dBatten_ptobatten00db_btobatten41db_PAon.dat'; sync_index = 30;
+%rx_filename = 'rx_file_30Sept2020_expC_noisegenon43dBatten_ptobatten00db_btobatten41db_PAon.dat'; sync_index = 30;
 
 %J/S 0 dB
-%rx_filename = 'rx_file_30Sept2020_expC_noisegenon43dBatten_ptobatten40db_btobatten41db_PAon.dat'; sync_index = 8; 
+rx_filename = 'rx_file_30Sept2020_expC_noisegenon43dBatten_ptobatten40db_btobatten41db_PAon.dat'; sync_index = 8; 
 
 
 srcdir = 'SourceData/TWGN_waveform';
@@ -63,22 +63,23 @@ y = read_interleaved_float32(seq_file,1e5,1e6);
 fs = 8*5/6*1e6; desiredfds = -10:0.1:10;
 options.doexciseband = 1; options.display = 1; options.update = 0;
 
-% non-linear predictor
+% non-linear predictor parameters
 params = [];
 params.domap = 1;
-params.hiddenSize = [4 2 2 1];
-%params.hiddenSize = [16 6 ]*16;  % wrks for ~7k weights
+params.hiddenSize = [16 8 4 2]; % hidden layer widths
 params.debugPlots=0;
-params.mu = 1e-3;
-params.trainFcn = 'trainlm';
-params.batchtype='fixed';
+params.mu = 1e-3;              % Levenberg-Marquardt step size
+params.trainFcn = 'trainlm';   % use LM
+params.batchtype='fixed';      % batch to use for train/test/validate
 params.initFcn = 'crandn'; % do not use 'c-nguyen-widrow';
 params.layersFcn = 'sigrealimag2';'cartrelu';'satlins';'satlins';
-params.outputFcn = 'purelin';
-params.nbrofEpochs = 200;
-params.mu_inc = 10;
-params.mu_dec = 1/10;
+params.outputFcn = 'purelin';  % purelin is typical
+params.nbrofEpochs = 200;      % training iterations
+params.mu_inc = 10;            % multiplier for mu when Hessian step increases mse
+params.mu_dec = 1/10;          % multiplier for mu when Hessian step decreases mse
 
+
+% samples to grab at a time
 len_x = 1e6;
 
 while(1)
@@ -101,6 +102,7 @@ while(1)
     lag = lags(peaks.indlag(1)); % pick the lag corresponding to max peak
     yd = transpose( delayseq( y(:), lag) );
     beta = yd(1:len_x)*yhat' /norm(yhat).^2;     
+    norm(beta*yhat(1:len_xt) - yd(1:len_xt))
     
         
     % setup the samples available for prediction (up to numlags delays)
@@ -125,6 +127,7 @@ while(1)
     lag = lags(peaks.indlag(1)); % pick the lag corresponding to max peak
     yd = transpose( delayseq( y(:), lag) );
     betanet = yd(1:len_xt)*yhatnet' /norm(yhatnet).^2;     
+    norm(betanet*yhatnet - yd(1:len_xt))    
     
     % complex net
     cnet = complexnet(params); disp('created complexnet, training...');
@@ -138,35 +141,48 @@ while(1)
     lag = lags(peaks.indlag(1)); % pick the lag corresponding to max peak
     yd = transpose( delayseq( y(:), lag) );
     betacnet = yd(1:len_xt)*yhatcnet' /norm(yhatcnet).^2;     
-
+    norm(betacnet*yhatcnet - yd(1:len_xt))    
+        
     lentoplot = min(1000,len_x);
     
     figure(101); clf;
     ha(1)=subplot(211);
     plot(real( alpha* xt(1:lentoplot)),'.-'); hold on;
     plot(real( rd(1:lentoplot)),'-');
+    legend('synced rx samples','tx interference');
+    grid minor; xlabel('samples'); ylabel('real');
     ha(2)=subplot(212);
     plot(imag( alpha* xt(1:lentoplot)),'.-'); hold on;
     plot(imag( rd(1:lentoplot)),'-');
+    grid minor; xlabel('samples'); ylabel('imag');
     linkaxes(ha,'x');
-    xlim([1 lentoplot]);
+    xlim([1 lentoplot]);    
 
-    norm(beta*yhat(1:len_xt) - yd(1:len_xt))
-    norm(betacnet*yhatcnet - yd(1:len_xt))    
         
     figure(102); clf;
     ha(1)=subplot(211);
     plot(real( beta* yhat(1:lentoplot)),'.-'); hold on;
     plot(real( betacnet* yhatcnet(1:lentoplot)),'.-'); 
-    plot(real( betanet* yhatnet(1:lentoplot)),'+-');     
+    plot(real( betanet* yhatnet(1:lentoplot)),'+-','MarkerSize',2);     
     plot(real( yd(1:lentoplot)),'-');
+    legend('linear estimate','complex ML','re/im ML','signal of interest');
+    grid minor; xlabel('samples'); ylabel('real');    
     ha(2)=subplot(212);
     plot(imag( beta* yhat(1:lentoplot)),'.-'); hold on;
     plot(imag( betacnet* yhatcnet(1:lentoplot)),'.-');
-    plot(imag( betanet* yhatnet(1:lentoplot)),'+-');    
-    plot(imag( yd(1:lentoplot)),'-');
+    plot(imag( betanet* yhatnet(1:lentoplot)),'+-','MarkerSize',2);    
+    plot(imag( yd(1:lentoplot)),'-');    
+    grid minor; xlabel('samples'); ylabel('imag');
     linkaxes(ha,'x');
     xlim([1 lentoplot]);
+    
+    figure(103);clf;
+    plot(( beta* yhat(1:lentoplot)),'.'); hold on;
+    plot(( betacnet* yhatcnet(1:lentoplot)),'.'); 
+    plot(( betanet* yhatnet(1:lentoplot)),'+','MarkerSize',2);     
+    plot(( yd(1:lentoplot)),'x','MarkerSize',2);
+    grid minor; xlabel('real'); ylabel('imag'); 
+    legend('linear estimate','complex ML','re/im ML','signal of interest');    
     
     pause;
     sync_index = sync_index + 1;    
