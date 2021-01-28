0e376643b197d29da91e5fe36e7da5ccd3dc7de5
Swaroop Appadwedula <swaroop@ll.mit.edu>
2021-01-28 09:18:33 -0500

    switch cases for jacobian,gradient with matlab

--- comparenets.m
+++ comparenets.m
@@ -1,14 +1,47 @@
 
-rng(101010);
+% need weight record check how weights are updated in matlab
+warning('Move trainlm_editedforweightRecord.m to trainlm.m first');
+pause;
 
-%DEFINE A SIMPLE PROBLEM
-in = (randi(2,2,10)-1.5)/2;
-%out = 2200*in(1,:).*in(2,:) +10010;
-out = in(1,:).*in(2,:)/100;
+rng(101);
 
-x = rand(2,1000)*371 - 5 ;
-in = x/10;
-out = 13* ( x(1,:).^2 + x(2,:).^2 );
+compareChoice = 'd';
+switch compareChoice
+    case 'a'
+        in = (randi(2,2,10)-1.5)/2;
+        %out = 2200*in(1,:).*in(2,:) +10010;
+        out = in(1,:).*in(2,:)/100;
+        Hidden_Neurons = [20 5 1];
+    case 'b'
+        x = rand(2,1000)*371 - 5 ;
+        in = x/10;
+        out = 13* ( x(1,:).^2 + x(2,:).^2 );
+
+        % two outputs
+        out = 13* [ x(1,:).^2; x(2,:).^2 ];
+
+        Hidden_Neurons = [20 5 1];
+        %Hidden_Neurons = [1];
+        %Hidden_Neurons = [];
+    case 'bb'
+        x = rand(1,1000)*371 - 5 ;
+        in = x/10;
+        out = 13* ( x(1,:).^2 );
+        Hidden_Neurons = [20 5 1];
+        Hidden_Neurons = [1];
+    case 'c'
+        %4e-8 error
+        in = 2*rand(4,100000)-1;
+        out = sin(2*pi*in(1,:)) .* in(2,:).^2 .* in(3,:).^3 .* in(4,:).^4 .* ...
+            exp(-1*(in(1,:)+in(2,:)+in(3,:)+in(4,:)));
+        Hidden_Neurons = [4 50];
+    case 'd'
+        %load run4;
+        in = realifyfn(intrain);
+        out = realifyfn(outtrain);
+        Hidden_Neurons = [8 4 1];
+end
+
 
 rng(randi(100,1,1));
 
@@ -18,8 +51,8 @@ valInd = trainInd(end) + (1:floor(0.15*L));
 testInd = (valInd(end) +1):L;
 
 % neural networks parameters
-Hidden_Neurons = [20 5 1];
-params.domap = 1;
+
+params.domap = 'reim';
 params.hiddenSize = [Hidden_Neurons];
 params.debugPlots=0;
 params.mu = 1e-3;
@@ -49,6 +82,7 @@ net.divideFcn='divideind';
 net.divideParam.trainInd=trainInd;
 net.divideParam.valInd=valInd;
 net.divideParam.testInd=testInd;
+net.trainParam.epochs=5
 
 net = train(net,in,out);
 length(weightRecord)
@@ -57,6 +91,3 @@ cnet = complexnet(params);
 cnet = copynet(cnet,net,IW,LW,b,weightRecord,jeRecord,jjRecord,weightRecord2);
 cnet.debugCompare = 1
 cnet = cnet.train(in,out);
-
-
-
