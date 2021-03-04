1d477f3b091032486988c35b038f1b7c9db110ee
swaroop <swaroop@ll.mit.edu>
2021-03-04 11:57:42 -0500

    blah

---
+++ bfeq/setup_env.m
@@ -0,0 +1,10 @@
+addpath ../
+addpath ../gradient-descent/
+addpath ../bfeq
+
+%homedir = '/afs/mitll/usr/sw17362/public';
+homedir = '/home/gridsan/SW17362';
+addpath(genpath(fullfile(homedir,'MATLAB')));  % computexcorr, boldify
+
+
+
